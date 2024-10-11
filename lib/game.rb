c0f63d7257d20c3292eb0d2435f937e94362f228
CodeByBlazej <codebyblazej@gmail.com>
2024-10-11 20:59:16 +0100

    read from file using while loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,11 @@
 puts "Game started!"
 
-lines = File.readlines('google-10000-english.txt')
-lines.each do |line|
-  puts line if line.length > 5 && line.length < 12
+chosen_word = nil
+path = ''
+f = File.open("google-10000-english.txt")
+
+while line = f.gets do
+  puts line if line.length > 5 && line.length <= 12
 end
+
+f.close
