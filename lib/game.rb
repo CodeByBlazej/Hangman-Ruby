a5c1ede400e505dfaa56c33b094a1cddf3d49155
CodeByBlazej <codebyblazej@gmail.com>
2024-10-12 08:05:18 +0100

    get us selected word from 5 to 12 letters

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,14 @@
 puts "Game started!"
 
-chosen_word = nil
-path = ''
-f = File.open("google-10000-english.txt")
 
-while line = f.gets do
-  puts line if line.length > 5 && line.length <= 12
+fname = 'google-10000-english.txt'
+words = []
+
+File.readlines(fname).each do |line|
+  words << line.chomp if line.size > 5 && line.size <= 12
 end
 
-f.close
+selected_word = words.sample
+p words
+p words.size
+p selected_word
