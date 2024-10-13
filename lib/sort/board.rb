82969792bfce8da3a17f5dd810b8a270a1e757ed
CodeByBlazej <codebyblazej@gmail.com>
2024-10-13 09:43:37 +0100

    add lives and improve hidden word method

--- lib/sort/board.rb
+++ lib/sort/board.rb
@@ -1,20 +1,36 @@
+
 class Board
-  attr_accessor :secret_word
+  attr_accessor :secret_word, :lives
 
   def initialize
     @secret_word = Array.new
-
+    @lives = Array.new(8, 'O')
   end
 
   def hidden_word(selected_word)
-    puts selected_word
+    if @secret_word.empty?
+      selected_word.split('').each { |letter| @secret_word.push('_') }
+      puts @secret_word.join(' ')
+    else
+      puts @secret_word.join(' ')
+    end
 
-    selected_word.split('').each { |letter| @secret_word.push('_') }
-    puts @secret_word.join(" ")
+  end
+
+  def show_lives
+    puts @lives.join(' ')
+  end
+
+  def remove_live
+    @lives.pop
+    show_lives
   end
 
   def display(selected_word)
+    puts "WORD\n"
     hidden_word(selected_word)
+    puts "YOUR LIVES"
+    show_lives
   end
 
 
